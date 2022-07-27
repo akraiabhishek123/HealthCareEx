45934c2bb10be63d04ffe7902a2964945943ff73
Abhishek Rai <akraiabhishek102@gmail.com>
2022-07-27 22:34:20 +0530

    health

---
+++ src/main/java/com/example/entity/Specialization.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="specialization_tab")
+public class Specialization {
+
+	@Id
+	@GeneratedValue = Generation
+}
+1:04:26
